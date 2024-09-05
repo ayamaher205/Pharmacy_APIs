f2c59764bdbc0abe9f96a8b6658aac53dbc3a1c6
ayamaher205 <aya.maher.m7md@gmail.com>
2024-09-05 15:25:28 +0300

    feat:update user controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,38 +1,25 @@
 /* eslint-disable camelcase */
 import { NextFunction, Request, Response } from "express";
+import * as bcrypt from "bcrypt";
 import prismaClient from "../app";
+import "dotenv/config";
 import asyncWrapper from "../utils/asyncWrapper";
 import DataBaseErr from "../errors/databaseError";
 import NotFoundErr from "../errors/notFoundError";
 
-const create = async (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.body);
-    const [user, err] = await asyncWrapper(prismaClient.user.create({
-        data:
-            req.body,
-    }));
-    if (err) {
-        return next(new DataBaseErr(`DataBase Error: ${err.message!}`));
-    }
-    if (!user) {
-        return next(new DataBaseErr("failed to create user!"));
-    }
-    return res.status(200).json(user);
-};
-const findUserById = async (req:Request, res:Response, next:NextFunction) => {
+const getUserById = async (req: Request, res: Response, next: NextFunction) => {
     const [user, err] = await asyncWrapper(prismaClient.user.findUnique(
-        { where: req.body.id },
+        { where: { id: Number(req.params.id) } },
     ));
     if (err) {
         return next(new DataBaseErr(`DataBase Error: ${err.message}`));
     }
     if (!user) {
-        // edit it
         return next(new NotFoundErr("there isn't user with this id"));
     }
     return res.status(200).json({ data: user });
 };
-const getAllUsers = async (req:Request, res:Response, next:NextFunction) => {
+const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
     const [users, err] = await asyncWrapper(prismaClient.user.findMany());
     if (err) {
         return next(new DataBaseErr(`DataBase Error: ${err.message}`));
@@ -40,13 +27,27 @@ const getAllUsers = async (req:Request, res:Response, next:NextFunction) => {
     return res.status(200).json({ data: users });
 };
 
-const deleteUser = async (req:Request, res:Response, next:NextFunction) => {
-    const [user, err] = await asyncWrapper(prismaClient.user.delete({ where: req.body.id }));
+const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+    const [user, err] = await asyncWrapper(prismaClient.user.delete(
+        { where: { id: req.body.id } },
+    ));
     if (err) {
         return next(new DataBaseErr(`DataBase Error: ${err.message}`));
     }
     return res.status(200).json({ data: user });
 };
+const updateUser = async (req: Request, res: Response, next: NextFunction) => {
+    req.body.password = await bcrypt.hash(req.body.password, 10);
+    const [updatedUser, err] = await asyncWrapper(prismaClient.user.update(
+        {
+            data: req.body,
+            where: { id: Number(req.params.id) },
+        },
+    ));
+    if (err) return next(err);
+    return res.status(200).json({ updatedUser });
+};
+
 export default {
-    create, findUserById, getAllUsers, deleteUser,
+    getUserById, getAllUsers, deleteUser, updateUser,
 };
